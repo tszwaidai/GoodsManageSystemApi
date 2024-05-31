915fec86eb536638677ffc21abfe9b691414ff21
tszwaidai <tszwaidai@163.com>
2024-05-31 21:36:28 +0800

    init

---
+++ src/main/java/com/gms/GmsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.gms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmsSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmsSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/gms/common/CodeGenerator.java
@@ -0,0 +1,11 @@
+package com.gms.common;
+
+/**
+ * @author tszwaidai
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/5/31 16:59
+ */
+public class CodeGenerator {
+
+}
