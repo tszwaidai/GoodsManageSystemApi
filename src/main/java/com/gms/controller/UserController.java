afe785f479b3edc94c08ecd535a4df278272cb7e
tszwaidai <tszwaidai@163.com>
2024-06-02 16:40:16 +0800

    修改

--- src/main/java/com/gms/controller/UserController.java
+++ src/main/java/com/gms/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.gms.controller;
 
 
+import com.gms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
 }
 
