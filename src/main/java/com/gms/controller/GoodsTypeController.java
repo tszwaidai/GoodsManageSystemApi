c3acb8fce84b40a1cf587a55474680daf5711605
tszwaidai <tszwaidai@163.com>
2024-06-12 00:28:35 +0800

    物品分类crud

--- src/main/java/com/gms/controller/GoodsTypeController.java
+++ src/main/java/com/gms/controller/GoodsTypeController.java
@@ -7,14 +7,12 @@ import com.gms.common.Result;
 import com.gms.entity.GoodsType;
 import com.gms.entity.User;
 import com.gms.service.GoodsTypeService;
+import io.swagger.models.auth.In;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -39,17 +37,63 @@ public class GoodsTypeController {
      * @param pageSize
      * @return
      */
-//    @GetMapping("/list")
-//    public Result getTypeList(@RequestParam(value = "typename",required = false) String typename,
-//                              @RequestParam(value = "pageNo") Long pageNo,
-//                              @RequestParam(value = "pageSize") Long pageSize) {
-//        LambdaQueryWrapper<GoodsType> wrapper = new LambdaQueryWrapper<>();
-//        //模糊查询
-//        wrapper.like(StringUtils.hasText(typename),GoodsType::getGoodsTypeName,typename);
-//        Page<User> page = new Page<>(pageNo,pageSize);
-//        goodsTypeService.page(page,wrapper);
-//        return Result.suc(page.getRecords(),page.getTotal());
-//    }
+    @GetMapping("/list")
+    public Result getTypeList(@RequestParam(value = "typename",required = false) String typename,
+                              @RequestParam(value = "pageNo") Long pageNo,
+                              @RequestParam(value = "pageSize") Long pageSize) {
+        LambdaQueryWrapper<GoodsType> wrapper = new LambdaQueryWrapper<>();
+        //模糊查询
+        wrapper.like(StringUtils.hasText(typename),GoodsType::getGoodsTypeName,typename);
+        Page<GoodsType> page = new Page<>(pageNo,pageSize);
+        goodsTypeService.page(page,wrapper);
+        return Result.suc(page.getRecords(),page.getTotal());
+    }
+
+    /**
+     * 新增物品分类
+     * @param goodsType
+     * @return
+     */
+    @PostMapping(value = "/add")
+    public Result addType(@RequestBody GoodsType goodsType) {
+        goodsTypeService.save(goodsType);
+        return Result.suc("新增物品分类成功");
+    }
+
+
+    /**
+     * 修改分类
+     * @param goodsType
+     * @return
+     */
+    @PutMapping(value = "/update")
+    public Result updateType(@RequestBody GoodsType goodsType){
+        goodsTypeService.updateById(goodsType);
+        return Result.suc("物品分类修改成功");
+    }
+
+    /**
+     * 根据ID获取分类信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public Result getTypeById(@PathVariable("id") Integer id) {
+        GoodsType goodsType = goodsTypeService.getById(id);
+        return Result.suc(goodsType);
+    }
+
+    /**
+     * 删除分类
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public Result deleteTypeById(@PathVariable("id") Integer id){
+        goodsTypeService.removeById(id);
+        return Result.suc("删除物品分类信息成功");
+    }
+
 
 }
 
